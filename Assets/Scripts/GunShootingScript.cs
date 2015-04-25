fa5e2e87c6b705413c7d7f74e285bc101e5f99b1
Widget <chrisdimauro@gmail.com>
2015-04-25 16:33:38 -0400

    input plugin, Proto-Player and Ray Gun Script
    cInput 2 plugin added for later.  Proto-player with placeholder
    graphics, Ray gun script.

---
+++ Assets/Scripts/GunShootingScript.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GunShootingScript : MonoBehaviour
+{
+	public int damagePerShot = 20;
+	public float timeBetweenBullets = 0.15f;
+	public float range = 100f;
+	
+	public Rigidbody bulletPrefab;
+	public float bulletSpeed = 10f;
+	
+	float timer;
+	Ray shootRay;
+	RaycastHit shootHit;
+	int shootableMask;
+	ParticleSystem gunParticles;
+	LineRenderer gunLine;
+	AudioSource gunAudio;
+	float effectsDisplayTime = 0.2f;
+	
+	
+	void Awake ()
+	{
+		shootableMask = LayerMask.GetMask ("Shootable");
+		gunParticles = GetComponent<ParticleSystem> ();
+		gunLine = GetComponent <LineRenderer> ();
+		gunAudio = GetComponent<AudioSource> ();
+	}
+	
+	
+	void Update ()
+	{
+		timer += Time.deltaTime;
+		
+		if(Input.GetButton ("Fire1") && timer >= timeBetweenBullets && Time.timeScale != 0)
+		{
+			Shoot ();
+		}
+		
+		if(timer >= timeBetweenBullets * effectsDisplayTime)
+		{
+			DisableEffects ();
+		}
+	}
+	
+	
+	public void DisableEffects ()
+	{
+		gunLine.enabled = false;
+	}
+	
+	
+	void Shoot ()
+	{
+		timer = 0f;
+		
+		gunAudio.Play ();
+		
+		//gunLight.enabled = true;
+		
+		gunParticles.Stop ();
+		gunParticles.Play ();
+		
+		gunLine.enabled = true;
+		gunLine.SetPosition (0, transform.position);
+		
+		shootRay.origin = transform.position;
+		shootRay.direction = transform.up;
+		
+		if(Physics.Raycast (shootRay, out shootHit, range, shootableMask))
+		{
+			
+			//CODE HERE TO DISTINGUISH BETWEEN PLAYERS/TOWERS/ETC
+			//PlayerHealth enemyHealth = shootHit.collider.GetComponent <PlayerHealth> ();
+//			if(enemyHealth != null)
+			{
+				//                enemyHealth.TakeDamage (damagePerShot);
+			}
+			gunLine.SetPosition (1, shootHit.point);
+		}
+		else
+		{
+			gunLine.SetPosition (1, shootRay.origin + shootRay.direction * range);
+		}
+		
+		//OR//
+		
+		//Rigidbody bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity) as Rigidbody;
+	//	bullet.GetComponent<Rigidbody>().AddForce(transform.forward * bulletSpeed);
+		
+		
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class PlayerMovement : MonoBehaviour{
+	// Normal Movements Variables
+	private float walkSpeed;
+	private float curSpeed;
+	private float maxSpeed;
+	private float sprintSpeed;
+	
+
+	void Start()
+	{
+
+		walkSpeed = 10.0f;
+		sprintSpeed = walkSpeed + (walkSpeed / 2);
+		
+	}
+	
+	void FixedUpdate()
+	{
+		curSpeed = walkSpeed;
+		maxSpeed = curSpeed;
+		
+		// Move 
+		GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Lerp(0, Input.GetAxis("Horizontal")* curSpeed, 0.8f),
+		                                   Mathf.Lerp(0, Input.GetAxis("Vertical")* curSpeed, 0.8f));
+		float rotateSpeed = 9999.0f;
+		//Vector3 moveDirection = gameObject.transform.position; 
+
+		Vector2 dir = GetComponent<Rigidbody2D>().velocity;
+
+
+		if(dir.magnitude > 0.0f){  //Replace this with input?
+		
+		//	print(dir.magnitude.ToString());
+		float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		//print (angle.ToString());
+		transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward), rotateSpeed * Time.deltaTime); 
+	}
+	}
+}
