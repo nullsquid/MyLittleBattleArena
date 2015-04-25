7e8016004120b0d832971b27a19ddb88a937eabc
Hyacinth <j.shepard.ramsay@gmail.com>
2015-04-25 14:29:37 -0400

    tower tracking

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -1,7 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum Team{
+	Red,
+	Blue
+};
 public abstract class Building : MonoBehaviour {
+
 	public float health;
 	//colliders?
 	//UI elements?
