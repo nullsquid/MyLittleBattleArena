5b7bbe87f0e90dbfd98728ac7a09b78e821f7d38
Hyacinth <j.shepard.ramsay@gmail.com>
2015-04-25 19:02:54 -0400

    added bullet class

--- Assets/Scripts/Barrel.cs
+++ Assets/Scripts/Barrel.cs
@@ -5,16 +5,33 @@ public class Barrel : MonoBehaviour {
 
 	public GameObject projectile;
 	public Tower tower;
+	public GameObject target;
+	float cooldown; 
 	// Use this for initialization
 	void Start () {
-		
+		cooldown = tower.cooldown;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		//if(tower.target!=null){
+		//	target = tower.target;
+		//}
+		if(tower.targets!=null){
+			target = tower.targets[0];
+			Track ();
+		}
+
 	}
-	void Shoot(GameObject target){
-		//logic for just firing the projectile
+	IEnumerator FireRate(float cooldown){
+
+		Instantiate(projectile, transform.position, transform.rotation);
+		yield return new WaitForSeconds(cooldown);
+	}
+	void Track(){
+		Vector3 dir = target.transform.position - transform.position;
+		float angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg);
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		//Shoot ();
 	}
 }
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -3,53 +3,78 @@ using System.Collections;
 using System.Collections.Generic;
 public class Tower : Building {
 
-	float cooldown;
+	public float cooldown;
 	float distanceToTarget;
-	GameObject target;
-	GameObject projectile;
+	//public GameObject target;
+	public GameObject projectile;
 	float projectileSpeed;
 	float range;
 	float rotateSpeed;
 	Vector2 relativePos;
 	public List<GameObject> targets = new List<GameObject>();
+
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+	//	hitColliders = Physics2D.OverlapCircle(transform.position, 1);
+		if(targets==null){
+			Idle ();
+		}
 
-
+	}
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.tag == "Targetable"){
+			targets.Add(other.gameObject);
+		}
 
 
 	}
-	void Track(){
+	void OnTriggerExit2D(Collider2D other){
+		if(other.gameObject == targets[0]){
+			targets.RemoveAt(0);
+		}
+		//else if (){
+
+		//}
+
+	}
+
+	void Idle(){
+
+	}
+	//void Track(GameObject target){
+	//
+	//}
+	/*void Track(GameObject target){
 
 		/*if(Vector2.Distance(gameObject.transform.position, this.transform.position) <= sight&&gameObject.tag == "Targetable"){
 			targets.Add(gameObject);
 
 		}*/
-		if(targets[0]!=null){
+
+		/*if(targets[0]!=null){
 			if(Vector2.Distance(targets[0].transform.position, this.transform.position) > sight){
 				targets.RemoveAt(0);
 			}
 		}
 		else if (targets[0] == null){
-			Idle();
-		}
+			Idle();*/
+}
 		//if(targets[0] != null){
 			
-			Quaternion.LookRotation(relativePos);
+		//	Quaternion.LookRotation(relativePos);
 			//Shoot (targets[0]);
 		//}
 
-	}
+	//}
 
-	void Shoot(GameObject target){
+	//void Shoot(GameObject target){
 
-	}
-	void Idle(){
+	//}
+//	void Idle(){
 
-	}
-}
+//	}
+//}
