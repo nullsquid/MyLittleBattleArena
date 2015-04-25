a7937cb87a3729ef4444bcef2371b8aa42439807
Hyacinth <j.shepard.ramsay@gmail.com>
2015-04-25 15:15:56 -0400

    turret tracking

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -6,9 +6,17 @@ public enum Team{
 	Blue
 };
 public abstract class Building : MonoBehaviour {
-
+	public float sight;
 	public float health;
 	//colliders?
 	//UI elements?
+	public void IsDamaged(int damage){
+		health -= damage;
+		if(health<=0){
+			IsDestroyed();
+		}
+	}
+	public void IsDestroyed(){
 
+	}
 }
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 public class Tower : Building {
 
 	float cooldown;
+	float distanceToTarget;
+	GameObject target;
+	GameObject projectile;
+	float projectileSpeed;
 	float range;
-
+	public List<GameObject> targets = new List<GameObject>();
 	// Use this for initialization
 	void Start () {
 	
@@ -15,8 +19,28 @@ public class Tower : Building {
 	void Update () {
 	
 	}
+	void Track(){
+
+		if(Vector2.Distance(gameObject.transform.position, this.transform.position) <= sight&&gameObject.tag == "Targetable"){
+			targets.Add(gameObject);
+		}
+		if(Vector2.Distance(targets[0].transform.position, this.transform.position) > sight){
+			targets.RemoveAt(0);
+		}
+		else if (targets[0] == null){
+			Idle();
+		}
+		if(targets[0] != null){
+
+			Shoot (targets[0]);
+		}
+
+	}
 
 	void Shoot(GameObject target){
+
+	}
+	void Idle(){
 
 	}
 }
