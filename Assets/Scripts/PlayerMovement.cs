1a600cf4eebfb35ca9350d31dec12988eceb95bb
Michael Consoli <michael@cartwheelgames.com>
2015-04-26 15:56:54 -0400

    Amended spawn point in player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -54,7 +54,7 @@ public class PlayerMovement : MonoBehaviour{  //this should probably be renamed
 	private float vertInput;
 	private bool fire = false;
 	private bool  canMove = false;
-	public Vector3 playerMovement;
+	public Vector3 spawnPosition;
 	public CharacterData thisCharacterData = new CharacterData(1,1,CharacterClass.Sniper,PlayerColor.red);
 	private Quaternion startRotation;
 	void Start()
@@ -226,7 +226,7 @@ public class PlayerMovement : MonoBehaviour{  //this should probably be renamed
 	
 	IEnumerator Respawn(){
 		
-		this.transform.position = playerMovement; //to be sure
+		this.transform.position = spawnPosition; //to be sure
 		
 		yield return new WaitForSeconds(1);
 	
