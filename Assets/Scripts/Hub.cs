243c4e644394fe696763a01290689464db93cbba
Hyacinth <j.shepard.ramsay@gmail.com>
2015-04-26 17:01:33 -0400

    began checking towers for hub invulnerability

--- Assets/Scripts/Hub.cs
+++ Assets/Scripts/Hub.cs
@@ -3,16 +3,26 @@ using System.Collections;
 using System.Collections.Generic;
 public class Hub : Building {
 	float range;
+	PlayerTeam team;
+	string teamColor;
 	List<GameObject> healTargets = new List<GameObject>();
+	public List<GameObject> turrets = new List<GameObject>();
 	float cooldown;
+	bool canBeDamaged;
 	public float maxCooldown = 2;
 	public float healStep = 1;
 	// Use this for initialization
 
-	
+	void Start(){
+		for(int i = 0; i <= team.buildings.Count; i++){
+			if(team.buildings[i].GetType() == typeof(Tower)){
+				turrets.Add(team.buildings[i].gameObject);
+			}
+		}
+	}
 	// Update is called once per frame
 	void Update () {
-	
+		
 	}
 
 	void Heal(GameObject target){
