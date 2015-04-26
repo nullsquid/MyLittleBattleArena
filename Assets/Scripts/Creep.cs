d046b198de24acbec34d1ccc5ea5224fc49556f1
Hyacinth <j.shepard.ramsay@gmail.com>
2015-04-26 11:29:09 -0400

    creep works in the center lane

--- Assets/Scripts/Creep.cs
+++ Assets/Scripts/Creep.cs
@@ -7,15 +7,32 @@ public enum Lane{
 }
 public class Creep : MonoBehaviour {
 	public string team;
+	public float speed = 2.0f;
 	public Transform target;
 	NavMeshAgent agent;
 	// Use this for initialization
 	void Start () {
-		agent = GetComponent<NavMeshAgent>();
+		//agent = GetComponent<NavMeshAgent>();
+		if(gameObject.tag == "blue"){
+		Vector3 dir = target.transform.position - transform.position;
+		float angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg) -90;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		agent.SetDestination(target.position);
+		//Vector3 dir = target.transform.position - transform.position;
+		//float angle = (Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg) +180;
+		//transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+		transform.Translate(Vector2.up * Time.deltaTime*speed);
+		//agent.SetDestination(target.position);
+	}
+	void OnCollisionEnter2D(Collider2D other){
+		if(other.gameObject.tag != this.gameObject.tag){
+			other.SendMessage("DealDamage", 1);
+
+		}
 	}
 }
