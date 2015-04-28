b2b9c184b5bb6bc09e8c73e1768db7a640abbfe1
Michael Consoli <michael@cartwheelgames.com>
2015-04-27 21:21:46 -0400

    Rewrote Character classes

--- Assets/Scripts/MapTile.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class MapTile : MonoBehaviour {
-	public int typeIndex;
-	public MapTile mirrorEquivalent;
-	public bool isTheMirrorVersion;
-}
---
+++ Assets/Scripts/Utility/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class GameManager : MonoBehaviour {
+	public static GameManager instance;
+	public bool matchInProgress = true;	//TODO false until the play button is pressed.
+	public static bool isInMatch {
+		get{
+			return instance.matchInProgress && !MapEditor.instance.inEditMode;
+		}
+	}
+	private void Awake(){
+		instance = this;
+	}
+}
